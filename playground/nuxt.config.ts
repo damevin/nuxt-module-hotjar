8ddea54649b47fdcef9d1541001d2fb0e4184b55
damevin <raphael@dame.vin>
2024-02-27 14:13:56 +0400

    fix: update config for playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,8 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   hotjar: {
-    hotjarId: '1234537',
-    scriptVersion: '6',
+    hotjarId: 1234533,
+    scriptVersion: 6,
     debug: false
   },
   devtools: { enabled: true }
