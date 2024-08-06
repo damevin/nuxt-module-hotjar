de87f35c6d609c5e67ca62a5954673318d1e2279
damevin <raphael@dame.vin>
2024-08-06 09:05:55 +0400

    chore: update deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
+
   hotjar: {
     hotjarId: 1234567,
     scriptVersion: 6,
     debug: true
   },
-  devtools: { enabled: true }
+
+  devtools: { enabled: true },
+  compatibilityDate: '2024-08-06'
 })
